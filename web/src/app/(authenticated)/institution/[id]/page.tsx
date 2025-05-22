5a032e74f0c2612415de944071fc5bcd62d4b804
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2025-05-22 00:24:52 -0300

    fix: deploy errors

--- web/src/app/(authenticated)/institution/[id]/page.tsx
+++ web/src/app/(authenticated)/institution/[id]/page.tsx
@@ -1,19 +1,23 @@
+"use client";
 import TableHeaderComponent from "@/components/table/tableHeader";
 import BottleCapCounter from "@/modules/app/BottleCapCount";
 import ControllersGrid from "@/modules/controllers/controllersGrid";
+import { useSearchParams } from "next/navigation";
 
-export default function ControllersPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const institutionId = Number(params?.id);
-
+export default function ControllersPage() {
+  const searchParams = useSearchParams();
+  const institutionId = Number(searchParams.get("institutionId"));
+  if (!institutionId) {
+    return <div>Instituição não encontrada</div>;
+  }
   return (
     <div>
       <BottleCapCounter institutionId={institutionId} />
       <br />
-      <TableHeaderComponent title="Lista de Totens" link={`/institution/${institutionId}/new`} />
+      <TableHeaderComponent
+        title="Lista de Totens"
+        link={`/institution/${institutionId}/new`}
+      />
       <ControllersGrid institutionId={institutionId} />
     </div>
   );

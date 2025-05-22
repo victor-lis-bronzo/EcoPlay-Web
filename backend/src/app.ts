a18166101164dbf3ef02e0e036a94ebc769fa7b1
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2025-05-22 00:28:17 -0300

    fix: deploy api

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -29,6 +29,7 @@ import fastifySwaggerUi from "@fastify/swagger-ui";
 const app = fastify();
 
 app.register(cors, {
+  origin: ["http://localhost:3000", "https://eco-play.vercel.app/"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   credentials: true, // Allow credentials if needed
 });
